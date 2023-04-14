d7756c83395a128cf335de25f24ef93229a3fbd8
liangzn <znliang@quant-chi.com>
2023-04-14 13:42:28 +0800

    feat: 登录页静态页面

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,3 +1,51 @@
+import { LockOutlined, UserOutlined } from '@ant-design/icons';
+import { Button, Checkbox, Form, Input } from 'antd';
+import styles from './index.less';
+
 export default function () {
-  return <div>login</div>;
+  const onFinish = (values: any) => {
+    console.log('Received values of form: ', values);
+  };
+  return (
+    <div className={styles.loginWrapper}>
+      <div className={styles.loginContainer}>
+        <Form
+          name="normal_login"
+          className={styles.loginForm}
+          initialValues={{ remember: true }}
+          onFinish={onFinish}
+        >
+          <Form.Item
+            name="username"
+            rules={[{ required: true, message: 'Please input your Username!' }]}
+          >
+            <Input
+              prefix={<UserOutlined className="site-form-item-icon" />}
+              placeholder="Username"
+            />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: 'Please input your Password!' }]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              type="primary"
+              htmlType="submit"
+              className={styles.loginFormButton}
+            >
+              登录
+            </Button>
+          </Form.Item>
+        </Form>
+      </div>
+    </div>
+  );
 }
