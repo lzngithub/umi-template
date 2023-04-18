fb49d3a92eae6afe05ee94e7fcf893d86452fb28
liangzn <znliang@quant-chi.com>
2023-04-18 16:45:46 +0800

    feat: 调整布局

--- config/router.ts
+++ config/router.ts
@@ -2,11 +2,13 @@ export default [
   { path: '/', redirect: '/login' }, // 首页默认跳转到登录页
   {
     path: '/login',
+    name: '登录',
     component: '@/pages/login/index',
   },
-  { path: '/home', component: '@/pages/home/index' },
+  { path: '/home', name: '首页', component: '@/pages/home/index' },
   {
     path: '/needAccess',
+    name: '权限校验页',
     component: '@/pages/needAccess/index',
   },
 ];
